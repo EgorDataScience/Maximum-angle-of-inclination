fdaa99dfe8ccec61eaa40a93b16e69b6485f8184
Egor <115636660+EgorDataScience@users.noreply.github.com>
2023-02-14 03:46:24 +0300

    Update differincial.py

--- differincial.py
+++ differincial.py
@@ -13,6 +13,4 @@ for i in range(n):
     r = (math.atan(abs(y2-y1)) * rad )    #r = the result is at one point
     d = d + r                             #d = sum of results
 print(d/n)                                #d/n = the limit value of the angle of inclination
-    
-
-
+  
